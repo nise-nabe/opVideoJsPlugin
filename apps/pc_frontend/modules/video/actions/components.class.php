8028bef5644b037026a2f253a7db3c45cce57047
watanabe <watanabe@openpne.jp>
2013-05-10 12:19:34 +0900

    add usage

--- apps/pc_frontend/modules/video/actions/components.class.php
+++ apps/pc_frontend/modules/video/actions/components.class.php
@@ -9,7 +9,22 @@
  */
 class videoComponents extends sfComponents
 {
+  /**
+   * play
+   *
+   * example:
+   *   include_component('video', 'play', array('filePath' => '/video.mp4'))
+   */
   public function executePlay()
   {
+    if (!isset($this->filePath))
+    {
+      $this->getLogger()->alert('no filePath was set.');
+    }
+
+    if ($this->filePath instanceof sfOutputEscaper)
+    {
+      $this->filePath = $this->filePath->getRawValue();
+    }
   }
 }
--- apps/pc_frontend/modules/video/templates/_play.php
+++ apps/pc_frontend/modules/video/templates/_play.php
@@ -2,6 +2,6 @@
 <?php $response->addJavascript('/opVideoJsPlugin/js/video.js') ?>
 
 <video id="home_video" class="video-js vjs-default-skin" controls preload="auto" width="640" height="360">
-<source src="<?php echo $path ?>" type='video/mp4' />
+<source src="<?php echo $filePath ?>" type='video/mp4' />
 </video>
 <script>var homePlayer=_V_("home_video");</script>
