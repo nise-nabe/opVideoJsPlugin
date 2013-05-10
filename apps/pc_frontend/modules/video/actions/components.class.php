2870b4098302a3afe18289928f068d8996d94185
watanabe <watanabe@openpne.jp>
2013-05-10 12:19:17 +0900

    add component and template

---
+++ apps/pc_frontend/modules/video/actions/components.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * videoComponents
+ *
+ * @package    OpenPNE
+ * @subpackage components
+ * @author     Yuya Watanabe <watanabe@openpne.jp>
+ */
+class videoComponents extends sfComponents
+{
+  public function executePlay()
+  {
+  }
+}
